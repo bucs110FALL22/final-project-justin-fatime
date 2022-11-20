a4adddde76312683ed1e1cac7fa8331b4512f1ce
Justin Tunney <112041098+jtunney1@users.noreply.github.com>
2022-11-20 22:25:01 +0000

    Moved information from controller to screens file

--- src/Controller.py
+++ src/Controller.py
@@ -19,45 +19,14 @@ class Controller:
 
   def menuloop(self):
     in_menu = True
-    
       #event loop
     while in_menu == True:
       #Create an instance on your controller object
       controller = Screens()
       #Call your mainloop
-      controller.update_menu(self.screen)
-      #update data
-      #create button locations and check if pressed
-      button_size = 50
-      size_list = pygame.display.get_window_size()
-      width = size_list[0]
-      height = size_list[1]
-      lvl_1_demensions = ((width/5)-(button_size/2),((height+100)/2)-(button_size/2),button_size,button_size)
-      lvl_2_demensions = (((2*width)/5)-(button_size/2),((height+100)/2)-(button_size/2),button_size,button_size)
-      lvl_3_demensions = (((3*width)/5)-(button_size/2),((height+100)/2)-(button_size/2),button_size,button_size)
-      lvl_4_demensions = (((4*width)/5)-(button_size/2),((height+100)/2)-(button_size/2),button_size,button_size)
-      lvl_1_select = pygame.Rect(lvl_1_demensions)
-      lvl_2_select = pygame.Rect(lvl_2_demensions)
-      lvl_3_select = pygame.Rect(lvl_3_demensions)
-      lvl_4_select = pygame.Rect(lvl_4_demensions)
-      exit_select = pygame.Rect(10,14,button_size/2,button_size/2)
-      for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-          mouse_click_pos = event.pos
-          if lvl_1_select.collidepoint(mouse_click_pos):
-            playerSelection = 1
-            in_menu = False
-          elif lvl_2_select.collidepoint(mouse_click_pos):
-            playerSelection = 2
-            in_menu = False
-          elif lvl_3_select.collidepoint(mouse_click_pos):
-            playerSelection = 3
-            in_menu = False
-          elif lvl_4_select.collidepoint(mouse_click_pos):
-            playerSelection = 4
-            in_menu = False
-          elif exit_select.collidepoint(mouse_click_pos):
-            pygame.quit()
+      playerSelection = controller.update_menu(self.screen)
+      if playerSelection != 0:
+        in_menu = False
       #redraw
       pygame.display.flip()
     return playerSelection
@@ -75,14 +44,8 @@ class Controller:
       controller = Screens()
       #Call your mainloop
       if in_pause == False:
-        controller.update_game(self.screen,game_level)
-        button_size = 50
-        pause_select = pygame.Rect(10,10,button_size/2,button_size/2)
-        for event in pygame.event.get():
-          if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_click_pos = event.pos
-            if pause_select.collidepoint(mouse_click_pos):
-              in_pause = True
+        in_pause = controller.update_game(self.screen,game_level)
+        
       elif in_pause == True:
         controller.update_game(self.screen,game_level)
         controller.update_pause(self.screen,game_level)
@@ -95,11 +58,10 @@ class Controller:
               in_game = False
             elif resume_select.collidepoint(mouse_click_pos):
               in_pause = False
-
       #update data
-
       #redraw
       pygame.display.flip()
+      
   def gameoverloop(self):
       #event loop
     while gameover == True:
--- src/Screens.py
+++ src/Screens.py
@@ -6,6 +6,7 @@ class Screens():
 
   def update_menu(self,screen):
     size_list = pygame.display.get_window_size()
+    playerSelection = 0
     width = size_list[0]
     height = size_list[1]
     screen.fill('light green')
@@ -50,10 +51,44 @@ class Screens():
     font = pygame.font.Font(None, 60)
     font_object = font.render(msg, True, "white")
     screen.blit(font_object, (((4*width)/5)-36,((height+110)/2)-(button_size/2)))
+    #update data
+      #create button locations and check if pressed
+    button_size = 50
+    size_list = pygame.display.get_window_size()
+    width = size_list[0]
+    height = size_list[1]
+    lvl_1_demensions = ((width/5)-(button_size/2),((height+100)/2)-(button_size/2),button_size,button_size)
+    lvl_2_demensions = (((2*width)/5)-(button_size/2),((height+100)/2)-(button_size/2),button_size,button_size)
+    lvl_3_demensions = (((3*width)/5)-(button_size/2),((height+100)/2)-(button_size/2),button_size,button_size)
+    lvl_4_demensions = (((4*width)/5)-(button_size/2),((height+100)/2)-(button_size/2),button_size,button_size)
+    lvl_1_select = pygame.Rect(lvl_1_demensions)
+    lvl_2_select = pygame.Rect(lvl_2_demensions)
+    lvl_3_select = pygame.Rect(lvl_3_demensions)
+    lvl_4_select = pygame.Rect(lvl_4_demensions)
+    exit_select = pygame.Rect(10,14,button_size/2,button_size/2)
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+          mouse_click_pos = event.pos
+          if lvl_1_select.collidepoint(mouse_click_pos):
+            playerSelection = 1
+            in_menu = False
+          elif lvl_2_select.collidepoint(mouse_click_pos):
+            playerSelection = 2
+            in_menu = False
+          elif lvl_3_select.collidepoint(mouse_click_pos):
+            playerSelection = 3
+            in_menu = False
+          elif lvl_4_select.collidepoint(mouse_click_pos):
+            playerSelection = 4
+            in_menu = False
+          elif exit_select.collidepoint(mouse_click_pos):
+            pygame.quit()
+    return playerSelection
 
   def update_game(self,screen,level):
     size_list = pygame.display.get_window_size()
     button_size = 50
+    in_pause = False
     width = size_list[0]
     height = size_list[1]
     background = 'light blue'
@@ -67,6 +102,15 @@ class Screens():
     #pause button
     pygame.draw.rect(screen,'black',(10,10,button_size/2,button_size/2))
     pygame.draw.rect(screen,background,(18.33,10,button_size/6,button_size/2))
+    button_size = 50
+    pause_select = pygame.Rect(10,10,button_size/2,button_size/2)
+    for event in pygame.event.get():
+          if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_click_pos = event.pos
+            if pause_select.collidepoint(mouse_click_pos):
+              in_pause = True
+    return in_pause
+    
   def update_pause(self,screen,level):
     size_list = pygame.display.get_window_size()
     width = size_list[0]
