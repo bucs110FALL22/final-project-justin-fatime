43512c0ac1de32a8d2884efbe9b7a520656ab537
Justin Tunney <112041098+jtunney1@users.noreply.github.com>
2022-11-20 22:11:07 +0000

    Went back to previous design

--- src/Controller.py
+++ src/Controller.py
@@ -1,6 +1,5 @@
 import pygame
 from Screens import Screens
-import math
 
 class Controller:
   
@@ -12,35 +11,17 @@ class Controller:
     self.screen = pygame.display.set_mode(size=(screen_demensions[0],screen_demensions[1]))
     
   def mainloop(self):
-    in_game = True
-    in_pause = False
-    playerSelection = 1
-    
-    in_menu = True
     #select state loop
-    while in_game == True:
-      if in_menu == True:
-        selection = self.menuloop()  
-        if selection == 1 or selection == 2 or selection == 3 or selection == 4:
-          in_menu = False
-          in_level = True     
-      elif in_level == True:
-        in_menu = self.gameloop(selection,in_pause)
-
-      pygame.display.flip()
-      
-    
-      
-    
-        
+    selection = self.menuloop()
+    self.gameloop(selection)
   
   ### below are some sample loop states ###
 
   def menuloop(self):
-    
+    in_menu = True
     
       #event loop
-    
+    while in_menu == True:
       #Create an instance on your controller object
       controller = Screens()
       #Call your mainloop
@@ -66,31 +47,30 @@ class Controller:
           if lvl_1_select.collidepoint(mouse_click_pos):
             playerSelection = 1
             in_menu = False
-            return playerSelection
           elif lvl_2_select.collidepoint(mouse_click_pos):
             playerSelection = 2
             in_menu = False
-            return playerSelection
           elif lvl_3_select.collidepoint(mouse_click_pos):
             playerSelection = 3
             in_menu = False
-            return playerSelection
           elif lvl_4_select.collidepoint(mouse_click_pos):
             playerSelection = 4
             in_menu = False
-            return playerSelection
           elif exit_select.collidepoint(mouse_click_pos):
             pygame.quit()
       #redraw
+      pygame.display.flip()
+    return playerSelection
       
-      
-  def gameloop(self,level,in_pause):
-    
+  def gameloop(self,level):
+    game_level = level
+    in_game = True
+    in_pause = False
+    size_list = pygame.display.get_window_size()
+    width = size_list[0]
+    height = size_list[1]
     #event loop
-      size_list = pygame.display.get_window_size()
-      game_level = level
-      width = size_list[0]
-      height = size_list[1]
+    while in_game == True:
       #Create an instance on your controller object
       controller = Screens()
       #Call your mainloop
@@ -112,15 +92,14 @@ class Controller:
           if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_click_pos = event.pos
             if exit_select.collidepoint(mouse_click_pos):
-              in_menu = True
-              return in_menu
+              in_game = False
             elif resume_select.collidepoint(mouse_click_pos):
               in_pause = False
 
       #update data
 
       #redraw
-      
+      pygame.display.flip()
   def gameoverloop(self):
       #event loop
     while gameover == True:
--- src/Screens.py
+++ src/Screens.py
@@ -1,5 +1,4 @@
 import pygame
-import math
 
 class Screens():
   def __init__(self):
@@ -59,10 +58,6 @@ class Screens():
     height = size_list[1]
     background = 'light blue'
     screen.fill(background)
-    #draw cannon base
-    pygame.draw.circle(screen,'gray',(100,height-50),50)
-    #draw ground - ground = 50 pixels off bottom
-    pygame.draw.rect(screen,'dark green',(0,height-50,width,50))
     #draw escape button
     #draw title
     msg = 'Level ' + str(level)
