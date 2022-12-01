c3cc7506a6d838b8e50cdfa5c0668ab424dccf0e
Fatime Useini <112587115+FatimeUseini@users.noreply.github.com>
2022-12-01 00:00:39 +0000

    Updated level 1 and 2

--- src/Enemy.py
+++ src/Enemy.py
@@ -7,13 +7,18 @@ class Enemy(pygame.sprite.Sprite):
      self.rect = self.image.get_rect()
      self.image = pygame.transform.scale(self.image,(40,40))
      self.image.set_colorkey((255,255,255))
-     self.rect = self.image.get_rect()
+     self.rect = self.image.get_rect(center= (x,y))
      self.rect.x = x
      self.rect.y = y
-     self.vel = 5
-
-     def update(self):
-       self.rect.x += self.vel
+       
+  def update(self):
+      self.rect.x -= 4
+      if self.rect.x == 0:
+        self.kill()
+      
+        
+        
+   
 
     
 
--- src/Screens.py
+++ src/Screens.py
@@ -1,7 +1,7 @@
 import pygame
 from src.Launcher import Launcher
-#from src.Enemy import Enemy
-#import random
+from src.Enemy import Enemy
+import random
 
 class Screens():
   def __init__(self):
@@ -35,14 +35,22 @@ class Screens():
 
   def update_game(self,screen,level):
     bg = pygame.image.load("assets/level"+ str(level)+"bg.png")  
+    clock = pygame.time.Clock()
     #creates sprites and sprite group 
     launcher_group = pygame.sprite.Group()
     launcher= Launcher(0,450)
     launcher_group.add(launcher)
     character_group = pygame.sprite.Group()
-    
-    
-    clock = pygame.time.Clock()
+    if str(level) == "1":
+      enemy_group= pygame.sprite.Group()
+      for i in range (0,5):
+        enemy = Enemy(800, random.randrange(80,400))
+        enemy_group.add(enemy)
+    if str(level) == "2":
+      for i in range (0,3):
+        enemy1_group = pygame.sprite.Group()
+        enemy1 = Enemy(800, random.randrange(80,400))
+        enemy1_group.add(enemy1)
     #launcher + character movement
     #pause button
     in_pause = False
@@ -55,32 +63,31 @@ class Screens():
             if pause_select.collidepoint(mouse_click_pos):
               in_pause = 'yes'
               run = False
-        # draws a new character when mouse clicked 
+        # draws a new character when spacebar is pressed
          key_input = pygame.key.get_pressed()   
          if key_input[pygame.K_SPACE]:
            character_group.add(launcher.draw_new_character())
           #check if enemy made it past character and if so the game is lost
-        # if enemy_group.pos.x == 400:
-         #  game = "lost"
-         #  return game 
+         #if enemy_group.rect.x == 400:
+           #game = "lost"
+           #return game 
           #check if game won
           #if enemies == 0:
            #game = 'won'
            #return game
-           
-           
       screen.blit(bg,(0,0))
+      if str(level) == "1":
+        enemy_group.draw(screen)
+        enemy_group.update()
+        pygame.sprite.groupcollide(character_group, enemy_group, True, True)
+      if str(level) == "2":
+        enemy1_group.draw(screen)
+        enemy1_group.update()
+        pygame.sprite.groupcollide(character_group, enemy1_group, True, True)
       character_group.draw(screen)
       launcher_group.draw(screen)
-     # enemy_group.draw(screen)
       character_group.update()
       launcher_group.update()
-      #if level == 1:
-          #enemy_group= pygame.sprite.Group()
-          #enemy = Enemy()
-          #enemy_group.add(enemy)
-          #enemy_group.draw(screen)
-      #pygame.sprite.groupcollide(character_group, enemy_group, True, True)
       pygame.display.flip()
       clock.tick(60)
     return in_pause
@@ -90,5 +97,6 @@ class Screens():
     screen.blit(pause,(225,100))
     
   def game_lost(self,screen,level):
-    pause = pygame.image.load("assets/pause.png")
-    screen.blit(pause,(225,100))
+      pause = pygame.image.load("assets/pause.png")
+      screen.blit(pause,(225,100))
+      
